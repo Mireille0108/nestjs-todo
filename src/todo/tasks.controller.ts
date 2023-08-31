0a62397db922f2fc8882c36149b49e129733473d
Mireille0108 <Embohmireill07@gmail.com>
2023-08-31 17:57:12 +0100

    my todo api

--- src/todo/tasks.controller.ts
+++ src/todo/tasks.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, ParseIntPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 // import { TaskModel } from './tasks.model';
 
 @Controller('tasks')
 export class TasksController {
-  _tasks: any;
+  // _tasks: any;
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
@@ -12,13 +12,14 @@ export class TasksController {
     return this.tasksService.findAllTasks();
   }
 
- @Get(':taskId')
-  getTaskById(@Param(':taskId')id: number) {
-    let task = this.tasksService.findTaskById(id);
-    
-    if(!task) {
-    throw new NotFoundException(`Task with id ${id} notb found`);
-    }
+  @Get(':id')
+  getTaskById(@Param('id', ParseIntPipe) id: number) {
+    const task = this.tasksService.findTaskById(id);
 
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    } else {
+      return task;
+    }
   }
 }
--- src/todo/tasks.service.ts
+++ src/todo/tasks.service.ts
@@ -3,21 +3,23 @@ import { TaskModel } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-  _tasks: TaskModel[] = [
-    { id: 1, description: 'Feed the cat', is_complete: false },
-    { id: 2, description: 'clean the window', is_complete: false },
+  private _tasks: TaskModel[] = [
+    { id: 1, description: 'Feed the cat', is_complete: true },
+    { id: 2, description: 'clean the window', is_complete: true },
     {
       id: 3,
       description: 'Finish recording YoutubebVideo',
-      is_complete: false,
+      is_complete: true,
     },
   ];
 
   findAllTasks(): TaskModel[] {
     return this._tasks;
   }
-  findTaskById(id: number): TaskModel {
-    return this._tasks.find((t)=> t.id == id);
 
+  findTaskById(id: number) {
+    const task2 = this._tasks.find((t) => t.id === id);
+    console.log('task2', task2);
+    return task2;
   }
 }
